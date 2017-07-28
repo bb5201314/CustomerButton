3df70176214b75d75fb4e69638e79ffc21df8fe3
xiaobo <970961800@qq.com>
2017-07-28 10:09:15 +0800

    Initial Commit

---
+++ CustomerButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomerButton
+//
+//  Created by 赵小波 on 2017/7/28.
+//  Copyright © 2017年 赵小波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomerButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomerButton
+//
+//  Created by 赵小波 on 2017/7/28.
+//  Copyright © 2017年 赵小波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
